429a98ec126bf37cdaa9b3faca5936c9829c2c16
Sadra <alizadeh.sadra.gh@gmail.com>
2026-01-20 20:07:48 +0300

    Add dynamic page titles and greeting to views
    Set ViewBag.Title in all main views and update layouts to use it for dynamic page titles. HomeController now provides a time-based greeting and user name via ViewBag, displayed on the Home/Index page.

--- MeetingApp/Controllers/HomeController.cs
+++ MeetingApp/Controllers/HomeController.cs
@@ -9,6 +9,13 @@ namespace MeetingApp.Controllers
         // localhost/Home/Index
         public IActionResult Index()
         {
+            int hour = System.DateTime.Now.Hour;
+
+            // ViewData["Greeting"] = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            ViewBag.User = "Sadra";
+
             return View();
         }
     }
