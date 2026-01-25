3a5374ccf1eef36896f16a433459362f8ff8a36e
Sadra <alizadeh.sadra.gh@gmail.com>
2026-01-25 13:51:50 +0300

    Add category filter to product listing
    Introduced a category dropdown filter to the product list on the Home page. Updated the controller to handle category filtering and pass the list of categories to the view.

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using FormsApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FormsApp.Controllers;
 
@@ -8,7 +9,7 @@ public class HomeController : Controller
 {
     public HomeController() { }
 
-    public IActionResult Index(string searchString)
+    public IActionResult Index(string searchString, string category)
     {
         var products = Repository.Products;
         if (!string.IsNullOrEmpty(searchString))
@@ -16,6 +17,14 @@ public class HomeController : Controller
             ViewBag.SearchString = searchString;
             products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
         }
+
+        if (!String.IsNullOrEmpty(category) && category != "0")
+        {
+            ViewBag.Category = category;
+            products = products.Where(p => p.CategoryId.ToString() == category).ToList();
+        }
+
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
         return View(products);
     }
 
