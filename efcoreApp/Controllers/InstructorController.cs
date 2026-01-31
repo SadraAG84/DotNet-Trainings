5766cc11a57946d9175182c02972036c4397d721
Sadra <alizadeh.sadra.gh@gmail.com>
2026-01-31 22:30:35 +0300

    Add Edit and Delete features for Instructor

--- efcoreApp/Controllers/InstructorController.cs
+++ efcoreApp/Controllers/InstructorController.cs
@@ -31,5 +31,75 @@ namespace efcoreApp.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var instructor = await _context.Instructors.FirstOrDefaultAsync(s =>
+                s.InstructorId == id
+            );
+            // var student = await _context.Students.FirstOrDefaultAsync(s => s.StudentId == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+            return View(instructor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Instructor model)
+        {
+            if (id != model.InstructorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Instructors.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Instructors.Any(s => s.InstructorId == model.InstructorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var instructor = await _context.Instructors.FindAsync(id);
+            // var instructor = await _context.Instructors.FirstOrDefaultAsync(s => s.InstructorId == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+            return View(instructor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete([FromForm] int id)
+        {
+            var instructor = await _context.Instructors.FindAsync(id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Instructors.Remove(instructor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
