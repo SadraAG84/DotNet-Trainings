efe7b44541fccd71b7495473f602772516ee62ce
Sadra <alizadeh.sadra.gh@gmail.com>
2026-02-04 19:39:38 +0300

    Add user creation flow (controller, VM, view)
    Introduce a user creation workflow: add CreateViewModel with validation, new Users/Create.cshtml view, and GET/POST Create actions in UsersController to create IdentityUser via UserManager. Also add a "Create New User" button to Users/Index and update _ViewImports to include IdentityApp.ViewModels and Identity EF imports. The app database files were updated as part of these changes.

--- IdentityApp/Controllers/UsersController.cs
+++ IdentityApp/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using IdentityApp.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +16,39 @@ namespace IdentityApp.Controllers
         public IActionResult Index()
         {
             return View(_userManager.Users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Create a new user with the provided username and email, and set the password
+                // The UserManager will handle password hashing and user creation
+                // If the user creation is successful, redirect to the Index action to show the list of users
+                // If there are errors during user creation, add them to the ModelState to display in the view
+                // Note: The UserManager will automatically handle password hashing and user creation
+                var user = new IdentityUser { UserName = model.UserName, Email = model.Email };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+                // The following line checks if the username (or email) is already taken:
+                // If the username is already taken, result.Succeeded will be false, and result.Errors will contain an error describing the issue.
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
