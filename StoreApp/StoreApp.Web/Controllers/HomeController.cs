d01c25913cabb81cf5ab5e2098df97f7999e83b6
Sadra <alizadeh.sadra.gh@gmail.com>
2026-02-09 19:38:49 +0300

    Add pagination support to product list
    Introduce pagination by adding a PageInfo class and embedding it in ProductListViewModel. HomeController now sets PageInfo (ItemsPerPage and TotalItems) and adjusts pageSize (2) when returning the product list. The Index view renders page links based on Model.PageInfo.TotalPages. This enables server-side paging and page navigation for the product listing.

--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using StoreApp.Web.Models;
 
 public class HomeController : Controller
 {
-    public int pageSize = 3;
+    public int pageSize = 2;
 
     // Dependency injection of the repository(we use it because we want to get the products from the database and show them in the view)(if we don't use it, we have to create an instance of the repository in the controller and that is not good because it will make the controller tightly coupled with the repository and that is not good for testing and maintenance)
     private readonly IStoreRepository _storeRepository;
@@ -15,7 +15,6 @@ public class HomeController : Controller
     {
         _storeRepository = storeRepository;
     }
-
 
     // localhost:5000/home/index?page=1
     public IActionResult Index(int page = 1)
@@ -32,6 +31,16 @@ public class HomeController : Controller
                 Category = p.Category,
             })
             .ToList();
-        return View(new ProductListViewModel { Products = products });
+        return View(
+            new ProductListViewModel
+            {
+                Products = products,
+                PageInfo = new PageInfo
+                {
+                    ItemsPerPage = pageSize,
+                    TotalItems = _storeRepository.Products.Count(),
+                },
+            }
+        );
     }
 }
